dc267eb55245e282edf9bf3eda0d4942441b67d2
damessina <58600552+damessina@users.noreply.github.com>
2020-03-10 13:38:07 +0100

    Update Mainprova.java

--- Mainprova.java
+++ Mainprova.java
@@ -16,25 +16,37 @@ public class MainProva {
 		System.out.println("3- Visualizza solo le aste al rialzo.");
 		System.out.println("4- Visualizza solo gli acquisti diretti.");
 		System.out.println("5- Visualizza gli annunci successivi ad una certa data.");
-		System.out.println("6- Termina le operazioni.");
+		System.out.println("6- Visualizza le aste scadute. ");
+		System.out.println("7- Termina le operazioni.");
 		System.out.println("");
 		
 		int scelta= input.nextInt();
 		input.nextLine();
 		switch (scelta) {
-			case 1 :	Date now =new Date();
+			case 1 :	System.out.println("- Crea un nuovo annuncio -");
+						System.out.println("");
+						Date now =new Date();
 						System.out.println("Aggiungi una breve descrizione del prodotto: ");
 						String desc=input.nextLine();
 						ciccio.aggiungiannuncio(desc, now);
 						break;
-			case 2: ciccio.visualizza(); break;
-			case 3: ciccio.visualizzaAste(); break;
-			case 4: ciccio.visualizzaDiretti(); break;
-			case 5: ciccio.visualizzaAfter(); break;
-			case 6: controllo=false; break;
+			case 2: System.out.println("- Visualizza tutti gli annunci -");
+					System.out.println("");
+					ciccio.visualizza(); break;
+			case 3: System.out.println("- Visualizza solo le aste al rialzo -");
+					ciccio.visualizzaAste(); break;
+			case 4: System.out.println("- Visualizza solo gli acquisti diretti -");
+					ciccio.visualizzaDiretti(); break;
+			case 5: System.out.println("- Visualizza solo gli acquisti diretti -");
+					ciccio.visualizzaAfter(); break;
+			case 6: System.out.println("- Visualizza le aste scadute -");
+					ciccio.visualizzascadute(); break;
+			case 7: System.out.println("- Termina le operazioni -");
+					controllo=false; break;
 			default : System.out.println("Errore nell'inserimento");
 		}
 		}while (controllo);
 		System.out.println("Grazie e Arrivederci.");
 	}
+
 }
