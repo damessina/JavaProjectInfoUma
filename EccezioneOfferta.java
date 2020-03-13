d0d285bfe06bfb197be1e64bc40e174ed9126316
damessina <58600552+damessina@users.noreply.github.com>
2020-03-13 22:20:44 +0100

    Create EccezioneOfferta.java

---
+++ EccezioneOfferta.java
@@ -0,0 +1,11 @@
+package eccezioni;
+
+public class EccezioneOfferta extends Exception {
+	
+	public EccezioneOfferta(String string){
+		super(string);
+	}
+	public EccezioneOfferta() {
+		super();
+	}
+}
