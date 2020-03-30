15c5e24250faeabff24bbb858908eafcb92c6398
damessina <58600552+damessina@users.noreply.github.com>
2020-03-31 01:28:29 +0200

    Update Mainprova.java

--- Mainprova.java
+++ Mainprova.java
@@ -1,35 +1,51 @@
-import java.util.Vector;
-
-import annunci.Annuncio;
-
-//import java.util.*;
-//import annunci.Annuncio;
-//import eccezioni.EccezioneDigitazione;
+import eccezioni.*;
+import java.util.*;
 
 public class MainProva {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
 		System.out.println("Benvenuto/a in Hasta L'Asta!");
-		//Scanner input = new Scanner(System.in);
 		
 		//accedi (Output) o registrati(Input)
-		//class UtenteRegistrato (nome, cognome, username, password)
-		//eccezione (if/else)
-		//Utente-->vettore
 		
+		//creo un vettore di utenti inizialmente vuoto che deve essere riempito
+		//una volta lanciato
 		Vector <Utente> utenti = new Vector <Utente>();
 		
-		// crea tre utenti impostando username e password
-		//Utente[] utenti = new Utente[3];
-		Utente tizio = new Utente("Tizio", "tizio", "123");
-		utenti.add(tizio);
-		Utente caio = new Utente("Caio", "caio", "456");
-		utenti.add(caio);
-		//utenti[2] = new Utente("Sempronio", "sempronio", "789");
+		//creo il menu delle aste
+		Menu mymenu=new Menu(utenti);
 		
-		Menu menu1=new Menu(<utenti> utenti);
-
-		System.out.println("Accedi alle aste online");
-				menu1.login();
+		boolean bandiera=true;
+		do {
+		if(!utenti.isEmpty()) {
+		System.out.println("Accedi [A] o Registrati [R]");
+		char scelta=input.next().charAt(0);
+		input.nextLine();
+		try {
+		if (scelta=='A') {
+			System.out.println("Accedi alle aste online");
+			bandiera=false;
+			mymenu.login();
+		}
+		else if(scelta=='R') {
+			
+			utenti.add(mymenu.creautente());
+			System.out.println("Registrazione eseguita con successo!");
+		}
+		else throw new EccezioneDigitazione ("Errore nella digitazione delle opzioni.");
+		}catch(EccezioneDigitazione e) {
+			System.out.println(e.getMessage());
+			System.out.println("Inserisci un carattere valido");
+		}
+		}
+		else {
+			System.out.println("Non ci sono utenti ancora registrati.");
+			System.out.println("Si proceda alla registrazione");
+			mymenu.creautente();
+			bandiera=false;
+		}
+		}while (bandiera);
+	input.close();
 	}
 }
